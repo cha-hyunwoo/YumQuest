4e7a4c339e4a929eea932530688af5a89e99e167
cha-hyunwoo <ckgusdn10@gmail.com>
2026-04-27 20:02:27 +0900

    feat: ProjectException 클래스 생성

---
+++ src/main/java/com/example/umc10th/global/apiPayload/exception/ProjectException.java
@@ -0,0 +1,7 @@
+package com.example.umc10th.global.apiPayload.exception;
+
+public class ProjectException extends RuntimeException {
+    public ProjectException(String message) {
+        super(message);
+    }
+}
