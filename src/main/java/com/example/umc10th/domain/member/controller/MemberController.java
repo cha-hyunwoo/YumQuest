ea6eff3dac1f9d690777d0a9d9300aa37feac3a5
cha-hyunwoo <ckgusdn10@gmail.com>
2026-04-28 10:02:09 +0900

    feat: MemberController 구현

--- src/main/java/com/example/umc10th/domain/member/controller/MemberController.java
+++ src/main/java/com/example/umc10th/domain/member/controller/MemberController.java
@@ -1,4 +1,32 @@
 package com.example.umc10th.domain.member.controller;
 
+import com.example.umc10th.domain.member.dto.MemberReqDTO;
+import com.example.umc10th.domain.member.dto.MemberResDTO;
+import com.example.umc10th.domain.member.service.MemberService;
+import com.example.umc10th.global.apiPayload.ApiResponse;
+import com.example.umc10th.global.apiPayload.code.BaseSuccessCode;
+import com.example.umc10th.global.apiPayload.code.MemberSuccessCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    // 마이페이지
+    @PostMapping("/v1/users/me")
+    public ApiResponse<MemberResDTO.GetInfo> getInfo(
+            // 받은 JSON 데이터를 자바 객체(dto)로 변환해서 씀
+            @RequestBody MemberReqDTO.GetInfo dto
+    ){
+        BaseSuccessCode code= MemberSuccessCode.OK;
+        return ApiResponse.onSuccess(code,memberService.getInfo(dto));
+    }
+
 }
