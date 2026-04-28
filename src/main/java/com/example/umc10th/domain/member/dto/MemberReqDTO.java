3705cf4999b9cee02e33f4d9f180f45fc8db804e
cha-hyunwoo <ckgusdn10@gmail.com>
2026-04-28 09:38:04 +0900

    feat: 마이페이지 조회를 위한 요청/응답 DTO 구현

--- src/main/java/com/example/umc10th/domain/member/dto/MemberReqDTO.java
+++ src/main/java/com/example/umc10th/domain/member/dto/MemberReqDTO.java
@@ -1,4 +1,9 @@
 package com.example.umc10th.domain.member.dto;
 
 public class MemberReqDTO {
+
+    // 마이페이지
+    public record GetInfo(
+            Long id
+    ){}
 }
--- src/main/java/com/example/umc10th/domain/member/dto/MemberResDTO.java
+++ src/main/java/com/example/umc10th/domain/member/dto/MemberResDTO.java
@@ -1,4 +1,15 @@
 package com.example.umc10th.domain.member.dto;
 
+import lombok.Builder;
+
 public class MemberResDTO {
+
+    @Builder
+    public record GetInfo(
+            String name,
+            String profileUrl,
+            String email,
+            String phoneNumber,
+            Integer point
+    ){}
 }
