b04eb0a8fa3b226bb20d969a0ab1150aac4fea25
cha-hyunwoo <ckgusdn10@gmail.com>
2026-04-30 13:31:28 +0900

    feat: 마이페이지 조회를 위한 MemberConverter 구현 및 DTO 매핑

--- src/main/java/com/example/umc10th/domain/member/converter/MemberConverter.java
+++ src/main/java/com/example/umc10th/domain/member/converter/MemberConverter.java
@@ -1,4 +1,20 @@
 package com.example.umc10th.domain.member.converter;
 
+import com.example.umc10th.domain.member.dto.MemberResDTO;
+import com.example.umc10th.domain.member.entity.Member;
+
 public class MemberConverter {
+    public static MemberResDTO.GetInfo toGetInfo(Member member) {
+        return MemberResDTO.GetInfo.builder()
+                .name(member.getName())
+                .gender(member.getGender())
+                .birth(member.getBirth())
+                .address(member.getAddress())
+                .detail_address(member.getDetailAddress())
+                .phoneNumber(member.getPhoneNumber())
+                .email(member.getEmail())
+                .point(member.getPoint())
+                .status(member.getStatus())
+                .build();
+    }
 }
