09620630fd0ce9d2c5cd0ad0a6ad37b7eee82685
cha-hyunwoo <ckgusdn10@gmail.com>
2026-04-30 14:02:22 +0900

    refactor: MemberException 생성자 파라미터 변경 (String -> MemberErrorCode)

--- src/main/java/com/example/umc10th/domain/member/exception/MemberException.java
+++ src/main/java/com/example/umc10th/domain/member/exception/MemberException.java
@@ -1,7 +1,18 @@
 package com.example.umc10th.domain.member.exception;
 
+import com.example.umc10th.domain.member.exception.code.MemberErrorCode;
+import lombok.Getter;
+
+@Getter
 public class MemberException extends RuntimeException {
-    public MemberException(String message) {
-        super(message);
+
+    // 에러 정보를 저장할 필드 추가
+    private final MemberErrorCode errorCode;
+
+    // MemberError를 인자로 받는 생성자 추가
+    public MemberException(MemberErrorCode errorCode) {
+        // 부모 클래스(RuntimeException)에게 에러 메시지를 전달
+        super(errorCode.getMessage());
+        this.errorCode=errorCode;
     }
 }
