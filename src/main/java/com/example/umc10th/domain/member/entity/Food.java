3bed9e9d09651445587839a6aaee349920a7c08f
cha-hyunwoo <ckgusdn10@gmail.com>
2026-04-30 14:48:10 +0900

    feat: Food 엔티티 생성

--- src/main/java/com/example/umc10th/domain/member/entity/Food.java
+++ src/main/java/com/example/umc10th/domain/member/entity/Food.java
@@ -1,4 +1,24 @@
 package com.example.umc10th.domain.member.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="food")
 public class Food {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
+    @Enumerated(EnumType.STRING)
+    private FoodName name;
 }
