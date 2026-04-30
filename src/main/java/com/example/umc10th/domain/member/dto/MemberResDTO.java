a825c88ba1aa418919c4196d4afcb8366206e008
cha-hyunwoo <ckgusdn10@gmail.com>
2026-04-30 13:32:38 +0900

    feat: MemberResDTO 응답 필드 추가

--- src/main/java/com/example/umc10th/domain/member/dto/MemberResDTO.java
+++ src/main/java/com/example/umc10th/domain/member/dto/MemberResDTO.java
@@ -2,14 +2,20 @@ package com.example.umc10th.domain.member.dto;
 
 import lombok.Builder;
 
+import java.time.LocalDate;
+
 public class MemberResDTO {
 
     @Builder
     public record GetInfo(
             String name,
-            String profileUrl,
-            String email,
+            Enum gender,
+            LocalDate birth,
+            String address,
+            String detailAddress,
             String phoneNumber,
-            Integer point
+            String email,
+            Integer point,
+            Enum status
     ){}
 }
