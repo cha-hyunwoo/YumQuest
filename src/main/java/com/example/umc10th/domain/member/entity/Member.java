b157ea068020f9c92d3a431e9cfd3453b9a1c882
cha-hyunwoo <ckgusdn10@gmail.com>
2026-04-30 14:48:50 +0900

    feat: Member 엔티티 구현

--- src/main/java/com/example/umc10th/domain/member/entity/Member.java
+++ src/main/java/com/example/umc10th/domain/member/entity/Member.java
@@ -1,4 +1,58 @@
 package com.example.umc10th.domain.member.entity;
 
-public class Member {
+import com.example.umc10th.domain.member.enums.Address;
+import com.example.umc10th.domain.member.enums.Gender;
+import com.example.umc10th.domain.member.enums.MemberStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="member")
+public class Member extends BaseEnTity{
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name="name", nullable=false)
+    private String name;
+
+    @Column(name="gender", nullable=false)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column(name="birth", nullable=false)
+    private LocalDate birth;
+
+    @Column(name="address", nullable=false)
+    @Enumerated(EnumType.STRING)
+    private Enum address;
+
+    @Column(name="detail_address", nullable=false)
+    private String detailAddress;
+
+    @Column(name="phoneNumber", nullable=false)
+    private String phoneNumber;
+
+    @Column(name="email", nullable=false)
+    private String email;
+
+    @Column(name="point", nullable=false)
+    @Builder.Default
+    private int point=0;
+
+    @Column(name="status", nullable=false)
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private MemberStatus status=MemberStatus.ACTIVE;
+
 }
