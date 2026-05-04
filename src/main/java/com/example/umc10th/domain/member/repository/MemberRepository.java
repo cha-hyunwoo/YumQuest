9e5e0e1bfe798be22eec432474a27a3355c969c2
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-04 17:21:19 +0900

    feat: MemberRepository에 활성 멤버 조회를 위한 커스텀 JPQL 추가

--- src/main/java/com/example/umc10th/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/umc10th/domain/member/repository/MemberRepository.java
@@ -2,6 +2,11 @@ package com.example.umc10th.domain.member.repository;
 
 import com.example.umc10th.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
+    @Query("SELECT m FROM Member m WHERE m.name=:name AND m.deletedAt IS NULL")
+    Optional<Member> findActiveMember(String name);
 }
