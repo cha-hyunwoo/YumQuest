589bb1106dc31270ebdb240d38526d28651e005d
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-05 14:26:08 +0900

    feat: MissionDTO 구현

--- src/main/java/com/example/umc10th/domain/mission/dto/MissionReqDTO.java
+++ src/main/java/com/example/umc10th/domain/mission/dto/MissionReqDTO.java
@@ -1,4 +1,8 @@
 package com.example.umc10th.domain.mission.dto;
 
 public class MissionReqDTO {
+    // 미션 도전하기 요청
+    public record ChallengeDTO(
+            Long memberId
+    ){}
 }
--- src/main/java/com/example/umc10th/domain/mission/dto/MissionResDTO.java
+++ src/main/java/com/example/umc10th/domain/mission/dto/MissionResDTO.java
@@ -1,4 +1,22 @@
 package com.example.umc10th.domain.mission.dto;
 
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
 public class MissionResDTO {
+    @Builder
+    public record MissionChallengeResult(
+            Long memberMissionId,
+            LocalDateTime createdAt
+    ){}
+
+    @Builder
+    public record MissionSummaryDTO(
+            Long missionId,
+            String storeName,    // 어떤 가게의 미션인지
+            Integer rewardPoint, // 보상 포인트
+            String content,      // 미션 내용
+            String status        // 현재 상태 (CHALLENGING 등)
+    ){}
 }
