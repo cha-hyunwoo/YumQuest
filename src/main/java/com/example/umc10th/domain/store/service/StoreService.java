0655241e2ecbe32041242fd27a05721a1715cfc9
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-05 14:50:15 +0900

    feat: StoreService 구현

---
+++ src/main/java/com/example/umc10th/domain/store/service/StoreService.java
@@ -0,0 +1,24 @@
+package com.example.umc10th.domain.store.service;
+
+import com.example.umc10th.domain.store.converter.StoreConverter;
+import com.example.umc10th.domain.store.dto.StoreResDTO;
+import com.example.umc10th.domain.store.entity.Store;
+import com.example.umc10th.domain.store.exception.StoreException;
+import com.example.umc10th.domain.store.exception.code.StoreErrorCode;
+import com.example.umc10th.domain.store.repository.StoreRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StoreService {
+    private final StoreRepository storeRepository;
+
+    public StoreResDTO.StoreInfo getStoreInfo(Long storeId) {
+        Store store = storeRepository.findById(storeId)
+                .orElseThrow(() -> new StoreException(StoreErrorCode.STORE_NOT_FOUND));
+        return StoreConverter.toStoreInfo(store);
+    }
+}
