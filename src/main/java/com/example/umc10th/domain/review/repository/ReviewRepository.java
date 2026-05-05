394e83535649ce996216de51d775e0564641452b
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-05 14:48:30 +0900

    feat: ReviewRepository 구현

--- src/main/java/com/example/umc10th/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/example/umc10th/domain/review/repository/ReviewRepository.java
@@ -1,4 +1,13 @@
 package com.example.umc10th.domain.review.repository;
 
-public class ReviewRepository {
+import com.example.umc10th.domain.review.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    // 특정 가게의 리뷰 목록 조회
+    @Query("SELECT r FROM Review r WHERE r.store.id = :storeId")
+    Page<Review> findAllByStoreId(Long storeId, Pageable pageable);
 }
