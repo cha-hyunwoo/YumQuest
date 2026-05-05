2d206304b3bacd5292387894eaded69cf982f445
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-05 14:48:38 +0900

    feat: ReviewDTO 구현

--- src/main/java/com/example/umc10th/domain/review/dto/ReviewReqDTO.java
+++ src/main/java/com/example/umc10th/domain/review/dto/ReviewReqDTO.java
@@ -1,12 +1,12 @@
 package com.example.umc10th.domain.review.dto;
 
-public class ReviewReqDTO {
+import java.util.List;
 
-    // 리뷰 쓰는 시점
-    public record PostReviewDTO(
-            Integer rating, // 사용자가 입력한 별점
-            String content, // 사용자가 작성한 리뷰
-            Long memberId, // 리뷰를 쓰는 사람의 ID
-            Long storeId // 어느 가게에 쓰는지에 대한 ID
+public class ReviewReqDTO {
+    public record CreateReview(
+            Long memberId,
+            Float rating,
+            String content,
+            List<String> imageUrls
     ){}
 }
--- src/main/java/com/example/umc10th/domain/review/dto/ReviewResDTO.java
+++ src/main/java/com/example/umc10th/domain/review/dto/ReviewResDTO.java
@@ -2,13 +2,22 @@ package com.example.umc10th.domain.review.dto;
 
 import lombok.Builder;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class ReviewResDTO {
-    // 리뷰를 보는 시점
     @Builder
-    public record PostReivewResultDTO(
-            Long reviewId, // 저장된 리뷰의 고유 ID
-            LocalDateTime createdAt // 서버에서 생성된 시간
+    public record CreateReviewResult(
+            Long reviewId,
+            LocalDateTime createdAt
+    ){}
+
+    @Builder
+    public record ReviewDetail(
+            Long reviewId,
+            String writerName,
+            Float rating,
+            String content,
+            LocalDate createdAt
     ){}
 }
