530e6011ef64fe6d0416f89a590d01708b86dd57
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-05 14:48:15 +0900

    feat: ReviewErrorCode 구현

--- src/main/java/com/example/umc10th/domain/review/exception/code/ReviewErrorCode.java
+++ src/main/java/com/example/umc10th/domain/review/exception/code/ReviewErrorCode.java
@@ -1,4 +1,15 @@
 package com.example.umc10th.domain.review.exception.code;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
 public enum ReviewErrorCode {
+    REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "REVIEW404_1", "해당 리뷰를 찾을 수 없습니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
 }
