a46792487395b19ad15c776ce3e553a2dc9ea9b8
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-05 14:25:02 +0900

    feat: MemberMission 구현

--- src/main/java/com/example/umc10th/domain/mission/entity/mapping/MemberMission.java
+++ src/main/java/com/example/umc10th/domain/mission/entity/mapping/MemberMission.java
@@ -1,4 +1,31 @@
 package com.example.umc10th.domain.mission.entity.mapping;
 
-public class MemberMission {
+import com.example.umc10th.domain.BaseEntity;
+import com.example.umc10th.domain.member.entity.Member;
+import com.example.umc10th.domain.mission.entity.Mission;
+import com.example.umc10th.domain.mission.enums.MissionStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberMission extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(15) DEFAULT 'CHALLENGING'")
+    private MissionStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "mission_id")
+    private Mission mission;
 }
