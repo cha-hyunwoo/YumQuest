b74a715667847d8cd9f05e27cb1a9b99c8d24c31
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-05 14:48:22 +0900

    feat: ReviewPicture 구현

--- src/main/java/com/example/umc10th/domain/review/entity/ReviewPicture.java
+++ src/main/java/com/example/umc10th/domain/review/entity/ReviewPicture.java
@@ -1,4 +1,23 @@
 package com.example.umc10th.domain.review.entity;
 
-public class ReviewPicture {
+import com.example.umc10th.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ReviewPicture extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
 }
