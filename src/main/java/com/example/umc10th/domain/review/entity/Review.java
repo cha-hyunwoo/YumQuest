590b98cf1aa79928beec0bee5ec37b57a411ad6c
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-05 14:47:49 +0900

    feat: Review 구현

--- src/main/java/com/example/umc10th/domain/review/entity/Review.java
+++ src/main/java/com/example/umc10th/domain/review/entity/Review.java
@@ -1,4 +1,37 @@
 package com.example.umc10th.domain.review.entity;
 
-public class Review {
+import com.example.umc10th.domain.BaseEntity;
+import com.example.umc10th.domain.member.entity.Member;
+import com.example.umc10th.domain.store.entity.Store;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Float rating;
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @OneToMany(mappedBy = "review", cascade = CascadeType.ALL)
+    private List<ReviewPicture> reviewImageList = new ArrayList<>();
 }
