d8eddb1d460a573a6e2838c0c0c5e6bae7304524
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-05 14:25:55 +0900

    feat: MissionRepository 구현

--- src/main/java/com/example/umc10th/domain/mission/repository/MissionRepository.java
+++ src/main/java/com/example/umc10th/domain/mission/repository/MissionRepository.java
@@ -1,4 +1,13 @@
 package com.example.umc10th.domain.mission.repository;
 
-public class MissionRepository {
+import com.example.umc10th.domain.mission.entity.Mission;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface MissionRepository extends JpaRepository<Mission, Long> {
+    // 특정 지역(Region)의 가게들에 걸린 미션 목록 조회
+    @Query("SELECT m FROM Mission m JOIN m.store s WHERE s.region.id = :regionId")
+    Page<Mission> findAllByRegionId(Long regionId, Pageable pageable);
 }
