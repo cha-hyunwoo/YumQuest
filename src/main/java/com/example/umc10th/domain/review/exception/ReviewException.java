67d788d79e730f1183b8423d7adc2474d4db44df
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-08 13:42:47 +0900

    feat: 리뷰 도메인 전용 예외 처리 및 에러 코드 수정

--- src/main/java/com/example/umc10th/domain/review/exception/ReviewException.java
+++ src/main/java/com/example/umc10th/domain/review/exception/ReviewException.java
@@ -1,7 +1,15 @@
 package com.example.umc10th.domain.review.exception;
 
+import com.example.umc10th.domain.review.exception.code.ReviewErrorCode;
+import lombok.Getter;
+
+@Getter
 public class ReviewException extends RuntimeException {
-    public ReviewException(String message) {
-        super(message);
+    // 에러 정보를 저장할 필드 추가
+    private final ReviewErrorCode  errorCode;
+    public ReviewException(ReviewErrorCode errorCode) {
+        // 부모 RuntimeException에 에러 메세지 전달
+        super(errorCode.getMessage());
+        this.errorCode=errorCode;
     }
 }
--- src/main/java/com/example/umc10th/domain/review/exception/code/ReviewErrorCode.java
+++ src/main/java/com/example/umc10th/domain/review/exception/code/ReviewErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ReviewErrorCode {
-    REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "REVIEW404_1", "해당 리뷰를 찾을 수 없습니다.");
+    REVIEW_NOT_CREATED(HttpStatus.NOT_FOUND, "REVIEW404_1", "해당 리뷰를 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String code;
