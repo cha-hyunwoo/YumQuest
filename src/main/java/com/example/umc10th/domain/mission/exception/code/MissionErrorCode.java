bd8dcdfee0239f30e3592bf556a66d012de2bb4a
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-10 22:27:07 +0900

    feat: 미션 에러 코드 추가

--- src/main/java/com/example/umc10th/domain/mission/exception/code/MissionErrorCode.java
+++ src/main/java/com/example/umc10th/domain/mission/exception/code/MissionErrorCode.java
@@ -1,4 +1,21 @@
 package com.example.umc10th.domain.mission.exception.code;
 
-public enum MissionErrorCode {
+import com.example.umc10th.global.apiPayload.code.BaseErrorCode;
+import com.example.umc10th.global.apiPayload.code.BaseSuccessCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+
+@Getter
+@RequiredArgsConstructor
+public enum MissionErrorCode implements BaseErrorCode {
+
+    MISSION_NOT_FOUND(HttpStatus.NOT_FOUND,
+            "MISSION404_1",
+            "미션 조회에 실패했습니다."),
+    ;
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
 }
