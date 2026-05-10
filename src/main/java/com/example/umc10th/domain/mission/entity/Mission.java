6968c9035c5ee68d26a724f30d4dbce539595660
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-10 22:26:07 +0900

    feat: status 컬럼 추가

--- src/main/java/com/example/umc10th/domain/mission/entity/Mission.java
+++ src/main/java/com/example/umc10th/domain/mission/entity/Mission.java
@@ -1,6 +1,7 @@
 package com.example.umc10th.domain.mission.entity;
 
 import com.example.umc10th.domain.BaseEntity;
+import com.example.umc10th.domain.mission.enums.MissionStatus;
 import com.example.umc10th.domain.store.entity.Store;
 import jakarta.persistence.*;
 import lombok.*;
@@ -24,4 +25,8 @@ public class Mission extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "store_id")
     private Store store;
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(15) DEFAULT 'READY'")
+    private MissionStatus status;
 }
