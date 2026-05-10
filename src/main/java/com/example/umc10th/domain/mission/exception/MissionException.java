91cf97b095325b68049256755f6a7b51e6ca565e
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-10 22:27:27 +0900

    feat: 에러 정보 String이 아닌 code로 받게 수정

--- src/main/java/com/example/umc10th/domain/mission/exception/MissionException.java
+++ src/main/java/com/example/umc10th/domain/mission/exception/MissionException.java
@@ -1,7 +1,19 @@
 package com.example.umc10th.domain.mission.exception;
 
+import com.example.umc10th.domain.member.exception.code.MemberErrorCode;
+import com.example.umc10th.domain.mission.exception.code.MissionErrorCode;
+import lombok.Getter;
+
+@Getter
 public class MissionException extends RuntimeException {
-    public MissionException(String message) {
-        super(message);
+
+    // 에러 정보를 저장할 필드 추가
+    private final MissionErrorCode errorCode;
+
+    // MemberError를 인자로 받는 생성자 추가
+    public MissionException(MissionErrorCode errorCode) {
+        // 부모 클래스(RuntimeException)에게 에러 메시지를 전달
+        super(errorCode.getMessage());
+        this.errorCode=errorCode;
     }
 }
