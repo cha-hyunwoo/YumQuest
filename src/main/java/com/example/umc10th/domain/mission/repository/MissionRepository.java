946ee708e78154c51070cf3d8c2dcb75d719d238
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-10 19:48:27 +0900

    refactor: MissionRepository 코드 삭제

--- src/main/java/com/example/umc10th/domain/mission/repository/MissionRepository.java
+++ src/main/java/com/example/umc10th/domain/mission/repository/MissionRepository.java
@@ -7,7 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
-    // 특정 지역(Region)의 가게들에 걸린 미션 목록 조회
-    @Query("SELECT m FROM Mission m JOIN m.store s WHERE s.region.id = :regionId")
-    Page<Mission> findAllByRegionId(Long regionId, Pageable pageable);
+
 }
