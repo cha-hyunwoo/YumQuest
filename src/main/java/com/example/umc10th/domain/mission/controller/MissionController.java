b57aa85b8ff7ec2c6a1dac254f8dba980176d07e
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-10 22:26:24 +0900

    refactor: uri 수정

--- src/main/java/com/example/umc10th/domain/mission/controller/MissionController.java
+++ src/main/java/com/example/umc10th/domain/mission/controller/MissionController.java
@@ -17,18 +17,28 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/members")
+@RequestMapping("/api/v1")
 public class MissionController {
     private final MissionService missionService;
 
     // 내가 진행중이거나 완료한 미션 목록 조회(페이징 포함)
-    @GetMapping("/{memberId}/missions")
+    @GetMapping("/members/{memberId}/missions")
     public ApiResponse<MissionResDTO.MissionListResDTO> getMemberMissions(
             @PathVariable Long memberId,
-            @ModelAttribute MissionReqDTO.MissionListReqDTO dto
+            @ModelAttribute MissionReqDTO.MissionListReqDTO dto // 데이터가 많을때
     ){
         // 서비스에서 데이터 가져오기
         return ApiResponse.onSuccess(MissionSuccessCode.OK, missionService.getMemberMissionList(memberId, dto));
     }
+
+    // 현재 선택된 지역에서 도전 가능한 미션 목록(페이징 포함)
+    @GetMapping("/regions/{regionId}/missions")
+    public ApiResponse<MissionResDTO.RegionMissionListDTO>getRegionMissions(
+            @PathVariable Long regionId,
+            @ModelAttribute MissionReqDTO.MyMissionReqDTO dto
+    ){
+        return ApiResponse.onSuccess(MissionSuccessCode.OK,missionService.getRegionMissionList(regionId,dto));
+    }
+
 
 }
