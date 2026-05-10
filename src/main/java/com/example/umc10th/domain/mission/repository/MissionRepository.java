e9c7ddffb89575b6a7a1ec3da4bed156c2dd1d9f
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-10 22:27:44 +0900

    feat: 쿼리 추가

--- src/main/java/com/example/umc10th/domain/mission/repository/MissionRepository.java
+++ src/main/java/com/example/umc10th/domain/mission/repository/MissionRepository.java
@@ -5,7 +5,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
 
+    @Query("SELECT m From Mission m "+
+    "JOIN FETCH m.store s "+
+    "JOIN s.region r "+
+    "WHERE r.id=:regionId "+
+    "AND m.status='READY'")
+    Page<Mission> findAllByRegionId(@Param("regionId")Long regionId, Pageable pageable);
 }
