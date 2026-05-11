17bf98ef9b7ec1f37dc51a47554c3708f85e7c19
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-11 18:54:41 +0900

    feat: BaseErrorCode 상속

--- src/main/java/com/example/umc10th/domain/store/exception/code/StoreErrorCode.java
+++ src/main/java/com/example/umc10th/domain/store/exception/code/StoreErrorCode.java
@@ -1,12 +1,14 @@
 package com.example.umc10th.domain.store.exception.code;
 
+import com.example.umc10th.global.apiPayload.code.BaseErrorCode;
+import com.example.umc10th.global.apiPayload.code.BaseSuccessCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
-public enum StoreErrorCode {
+public enum StoreErrorCode implements BaseErrorCode {
     STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "STORE404_1", "해당 가게를 찾을 수 없습니다."),
     REGION_NOT_FOUND(HttpStatus.NOT_FOUND, "STORE404_2", "해당 지역이 존재하지 않습니다.");
 
