7f4d4fa3c4b21d4ff5b6f390a4c6e23ef743a3ce
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-11 18:52:01 +0900

    feat: 슬라이스 커서 기반 페이지네이션 구현

--- src/main/java/com/example/umc10th/domain/mission/repository/MissionRepository.java
+++ src/main/java/com/example/umc10th/domain/mission/repository/MissionRepository.java
@@ -2,10 +2,14 @@ package com.example.umc10th.domain.mission.repository;
 
 import com.example.umc10th.domain.mission.entity.Mission;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
 
@@ -15,4 +19,8 @@ public interface MissionRepository extends JpaRepository<Mission, Long> {
     "WHERE r.id=:regionId "+
     "AND m.status='READY'")
     Page<Mission> findAllByRegionId(@Param("regionId")Long regionId, Pageable pageable);
+
+    Slice<Mission> findMissionByStore_IdOrderByIdDesc(Long storeId, PageRequest pageRequest);
+
+    Slice<Mission> findMissionByStore_IdAndIdLessThanOrderByIdDesc(Long storeId, long idCursor, PageRequest pageRequest);
 }
