833191230752e58a2aaed667bb263e37b1840b0b
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-11 15:15:33 +0900

    feat: Enum 임의 값 추가

--- src/main/java/com/example/umc10th/domain/member/enums/Address.java
+++ src/main/java/com/example/umc10th/domain/member/enums/Address.java
@@ -1,4 +1,7 @@
 package com.example.umc10th.domain.member.enums;
 
 public enum Address {
+    SEOUL,
+    GYEONGGI,
+    INCHEON
 }
--- src/main/java/com/example/umc10th/domain/member/enums/FoodName.java
+++ src/main/java/com/example/umc10th/domain/member/enums/FoodName.java
@@ -1,4 +1,8 @@
 package com.example.umc10th.domain.member.enums;
 
 public enum FoodName {
+    KOREAN,
+    JAPANESE,
+    CHINESE,
+    WESTERN
 }
