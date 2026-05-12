d2c56ffbe01a896e887539f5cfd0a580b069e9b0
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-12 13:50:46 +0900

    feat: 리뷰 조회 에러와 성공 코드 추가

--- src/main/java/com/example/umc10th/domain/review/exception/code/ReviewErrorCode.java
+++ src/main/java/com/example/umc10th/domain/review/exception/code/ReviewErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ReviewErrorCode {
-    REVIEW_NOT_CREATED(HttpStatus.NOT_FOUND, "REVIEW404_1", "해당 리뷰를 찾을 수 없습니다.");
+    REVIEW_NOT_CREATED(HttpStatus.NOT_FOUND, "REVIEW404_1", "해당 리뷰를 찾을 수 없습니다."),
+    QUERY_NOT_VALID(HttpStatus.BAD_REQUEST, "REVIEW400_1", "유효하지 않은 쿼리 파라미터입니다.");
 
     private final HttpStatus status;
     private final String code;
--- src/main/java/com/example/umc10th/domain/review/exception/code/ReviewSuccessCode.java
+++ src/main/java/com/example/umc10th/domain/review/exception/code/ReviewSuccessCode.java
@@ -8,11 +8,13 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ReviewSuccessCode implements BaseSuccessCode {
-
-    OK(HttpStatus.OK,
+    CREATED(HttpStatus.CREATED,
             "REVIEW200_1",
-            "성공적으로 리뷰가 작성되었습니다."),
-    ;
+            "성공적으로 리뷰가 생성되었습니다."),
+    OK(HttpStatus.OK,
+            "REVIEW200_2",
+            "성공적으로 리뷰가 조회되었습니다."),
+            ;
     private final HttpStatus status;
     private final String code;
     private final String message;
