c26ed4726bab3a08ca700406f03209f37bd22f99
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-12 13:50:06 +0900

    feat: 페이지네이션 틀, 리뷰 상세정보 ReviewResDTO 작성

--- src/main/java/com/example/umc10th/domain/review/dto/ReviewResDTO.java
+++ src/main/java/com/example/umc10th/domain/review/dto/ReviewResDTO.java
@@ -4,11 +4,32 @@ import lombok.Builder;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ReviewResDTO {
     @Builder
     public record CreateReviewResult(
             Long id,
             LocalDateTime createdAt
+    ){}
+
+    // 전체 목록 응답
+    @Builder
+    public record MyReviewDetailDTO(
+            Long reviewId,
+            String storeName,
+            String memberName,
+            Float rating,
+            String content,
+            LocalDate createdAt
+    ){}
+
+    // 페이지네이션 틀
+    @Builder
+    public record Pagination<T>(
+            List<T> data,
+            Boolean hasNext,
+            String nextCursor,
+            Integer pageSize
     ){}
 }
