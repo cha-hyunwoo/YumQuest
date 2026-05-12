08c9cb8572f6cfc142b13356cdd1bc5a68bc7178
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-12 14:06:50 +0900

    feat: RequestBody로 받을 memberId추가

--- src/main/java/com/example/umc10th/domain/mission/dto/MissionReqDTO.java
+++ src/main/java/com/example/umc10th/domain/mission/dto/MissionReqDTO.java
@@ -15,26 +15,30 @@ public class MissionReqDTO {
     // 미션 목록 조회
     @Builder
     public record MissionListReqDTO(
-        MissionStatus status,
-        Integer page
-    ){}
+            Long memberId,
+            MissionStatus status,
+            Integer page
+    ) {
+    }
 
     // 홈 화면 가능 미션 목록
     @Builder
     public record MyMissionReqDTO(
             Long regionId,
             Integer page
-    ){}
+    ) {
+    }
 
     // 가게 미션 생성
     @Builder
-    public record  CreateMission(
-            @NotBlank(message="조건은 빈칸일 수 없습니다.")
+    public record CreateMission(
+            @NotBlank(message = "조건은 빈칸일 수 없습니다.")
             String content,
-            @NotNull(message="마감기한은 필수입니다.")
+            @NotNull(message = "마감기한은 필수입니다.")
             LocalDate deadline,
-            @NotNull(message="미션 성공 포인트는 필수입니다.")
+            @NotNull(message = "미션 성공 포인트는 필수입니다.")
             Integer rewardPoint,
             MissionStatus status
-    ){}
+    ) {
+    }
 }
