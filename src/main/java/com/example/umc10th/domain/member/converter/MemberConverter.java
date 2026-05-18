b7312e3b7d7aeb120a6e6122ab800fb337435a2f
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-18 15:58:31 +0900

    feat: 회원가입 toMember, toSignUp 컨버터 메서드 작성

--- src/main/java/com/example/umc10th/domain/member/converter/MemberConverter.java
+++ src/main/java/com/example/umc10th/domain/member/converter/MemberConverter.java
@@ -1,5 +1,6 @@
 package com.example.umc10th.domain.member.converter;
 
+import com.example.umc10th.domain.member.dto.MemberReqDTO;
 import com.example.umc10th.domain.member.dto.MemberResDTO;
 import com.example.umc10th.domain.member.entity.Member;
 
@@ -16,5 +17,23 @@ public class MemberConverter {
                 .point(member.getPoint())
                 .status(member.getStatus())
                 .build();
+    }
+
+    // DTO -> Entity 변환
+    public static Member toMember(MemberReqDTO.SignUpReqDTO dto, String encodedPassword) {
+        return Member.builder()
+                .name(dto.name())
+                .gender(dto.gender())
+                .birth(dto.birth())
+                .address(dto.address())
+                .detailAddress(dto.detailAddress())
+                .phoneNumber(dto.phoneNumber())
+                .email(dto.email())
+                .password(encodedPassword) // BCrypt 암호화된 비밀번호
+                .build();
+    }
+
+    public static MemberResDTO.SignUpResDTO toSignUpResDTO(Member member) {
+        return new MemberResDTO.SignUpResDTO(member.getId());
     }
 }
