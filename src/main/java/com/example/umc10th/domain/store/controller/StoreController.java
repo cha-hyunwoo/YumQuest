42bc004683fdc377a5bdb302a1e8aa84c660d70f
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-18 15:55:41 +0900

    chore: StoreController 미사용 import 제거

--- src/main/java/com/example/umc10th/domain/store/controller/StoreController.java
+++ src/main/java/com/example/umc10th/domain/store/controller/StoreController.java
@@ -1,10 +1,5 @@
 package com.example.umc10th.domain.store.controller;
 
-import com.example.umc10th.domain.store.dto.StoreResDTO;
-import com.example.umc10th.domain.store.service.StoreService;
-import com.example.umc10th.global.apiPayload.ApiResponse;
-import com.example.umc10th.global.apiPayload.code.BaseSuccessCode;
-import com.example.umc10th.global.apiPayload.code.MemberSuccessCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
