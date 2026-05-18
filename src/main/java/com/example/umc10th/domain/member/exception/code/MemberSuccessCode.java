c4b729888ae80c572de69a9633fddc2bef7716cc
cha-hyunwoo <ckgusdn10@gmail.com>
2026-05-18 15:54:25 +0900

    refactor: 전역 MemberSuccessCode를 Member 도메인으로 통합

--- src/main/java/com/example/umc10th/domain/member/exception/code/MemberSuccessCode.java
+++ src/main/java/com/example/umc10th/domain/member/exception/code/MemberSuccessCode.java
@@ -1,4 +1,23 @@
 package com.example.umc10th.domain.member.exception.code;
 
-public enum MemberSuccessCode {
+import com.example.umc10th.global.apiPayload.code.BaseSuccessCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum MemberSuccessCode implements BaseSuccessCode {
+
+    OK(HttpStatus.OK,
+            "MEMBER200_1",
+            "성공적으로 유저를 조회했습니다."),
+    SIGN_UP(HttpStatus.OK,
+            "MEMBER200_2",
+            "회원가입에 성공했습니다.");
+
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
 }
